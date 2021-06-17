66ba5a47b40abe5897c0b8f7e6213d0cc1b81489
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2021-06-17 22:22:43 +0530

    Add files via upload

---
+++ Area or Perimeter Problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,area,peri;
+	cin>>a>>b;
+	area=a*b;
+	peri=2*(a+b);
+	if(area>peri){
+		cout<<"Area"<<"\n"<<area;
+	}
+	else{
+		cout<<"Peri"<<"\n"<<peri;
+	}
+	return 0;
+}
